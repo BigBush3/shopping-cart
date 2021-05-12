8631e3b3dc41474be80b8062550ae68b1bb7ef55
Andrey <andrey.platonov2005@gmail.com>
2021-05-12 17:46:49 +0300

    project was done
    It is an easy project just for experience and nothing bigger. I was so tired and nothing share for weeks. But I have a lot of stuff to share with github account

---
+++ src/data/items.js
@@ -0,0 +1,35 @@
+const items = [
+	{
+		id: 1,
+		name: 'Comfy Slippers',
+		price: 12.00,
+		category: 'Footwear',
+		description: 'I thing that it is the best slippers in the world'
+	}, {
+		id: 2,
+		name: 'Comfy Slippers',
+		price: 12.00,
+		category: 'Footwear',
+		description: 'I thing that it is the best slippers in the world'
+	}, {
+		id: 3,
+		name: 'Comfy Slippers',
+		price: 12.00,
+		category: 'Footwear',
+		description: 'I thing that it is the best slippers in the world'
+	}, {
+		id: 4,
+		name: 'Comfy Slippers',
+		price: 12.00,
+		category: 'Footwear',
+		description: 'I thing that it is the best slippers in the world'
+	}, {
+		id: 5,
+		name: 'Comfy Slippers',
+		price: 12.00,
+		category: 'Footwear',
+		description: 'I thing that it is the best slippers in the world'
+	},
+]
+
+export default items
